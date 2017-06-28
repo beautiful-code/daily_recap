41d1adc6c60b46107611bb704d0b5aafd7fd3832
saichander <saichander.ramaram@gmail.com>
2017-06-28 21:36:07 +0530

    code review comments 28th June

--- app/controllers/daily_logs_controller.rb
+++ app/controllers/daily_logs_controller.rb
@@ -6,7 +6,9 @@ class DailyLogsController < ApplicationController
   def new
     @daily_log =current_user.daily_logs.new
     @projects=Project.all
+    #TODO call build_query_hash in user_create_summary method itself
     query_hash = DailyLog.build_query_hash(params,current_user.id)
+    #TODO empty space before and after =
     @user_log_summary=DailyLog.user_create_summary(query_hash,params[:project_id],nil)
   end
 
@@ -14,7 +16,9 @@ class DailyLogsController < ApplicationController
     if(DailyLog.where(user_id: current_user.id,log_date:params[:datetime_ida]).count==0)
       begin
         ActiveRecord::Base.transaction do
-          daily_log=DailyLog.create(user_id: current_user.id,takeaway: params[:takeaway],log_date: params[:datetime_ida])
+          #TODO which one do you think is better?
+          daily_log = current_user.daily_logs.create(takeaway: params[:takeaway],log_date: params[:datetime_ida])
+          #daily_log=DailyLog.create(user_id: current_user.id,takeaway: params[:takeaway],log_date: params[:datetime_ida])
           params[:log_entry].each do |project_id,log_texts|
             log_texts.each do |logtext|
               daily_log.log_entries.create(project_id:project_id.to_i,log_text:logtext)
@@ -22,6 +26,7 @@ class DailyLogsController < ApplicationController
           end
         end
       rescue
+        #TODO it should be flash[:error]
         flash[:info]= "Error occured,please try again"
         redirect_to new_daily_log_path
       end
@@ -36,8 +41,6 @@ class DailyLogsController < ApplicationController
   def index
   end
 
-  #TODO change this action to index action
-
   def people_logs
     @log_summary=DailyLog.user_create_summary(nil,nil,Date.today)
     @all_users=User.all;
@@ -45,16 +48,20 @@ class DailyLogsController < ApplicationController
 
   def refresh
     @log_summary=DailyLog.user_create_summary(nil,nil, params[:log_date])
+    #TODO why are you sending log_summary twice
     render :partial => "daily_logs/user_summary",object: @log_summary,locals: { log_summary: @log_summary}
   end
 
   def user_logs
+    #TODO call build_query_hash in user_create_summary method itself
     query_hash = DailyLog.build_query_hash(params,current_user.id)
     @user_logs=DailyLog.user_create_summary(query_hash,params[:project_id],nil)
+    #TODO why are you sending user_logs twice
     render :partial => "daily_logs/user_summary",object: @user_logs,locals: { log_summary: @user_logs}
   end
 
   def edit_log
+    #TODO params should be params[:daily_log_id]
     @edit_log = DailyLog.find(params[:log_id]).create_record(nil)
     @projects=Project.all
     @daily_log_id=params[:log_id]
@@ -67,6 +74,7 @@ class DailyLogsController < ApplicationController
         if(!params[:new_log_entries].nil?)
           params[:new_log_entries].each do |project_id,log_entries|
             log_entries.each do|logtext|
+              #TODO no find_by_id, use find
               DailyLog.find_by_id(params[:daily_log_id]).log_entries.create(project_id:project_id,log_text:logtext) 
             end
           end
--- app/models/daily_log.rb
+++ app/models/daily_log.rb
@@ -3,12 +3,16 @@ class DailyLog < ApplicationRecord
   belongs_to :user
   include ApplicationHelper
 
+  #TODO create_user_summary would have been a better name
+  #TODO golden rule: always space after a comma
   def self.user_create_summary(query_hash,project_id,log_date)
+    #TODO project_id.present? is same as !project_id.nil?
     if(!project_id.nil? and !project_id.blank?)
       daily_logs= DailyLog.where(query_hash).joins(:log_entries).where(log_entries: {project_id: project_id}).order("log_date DESC").distinct
     else
       daily_logs=DailyLog.where(query_hash).order("log_date DESC")
     end
+    #TODO query_hash.nil? will return false even if query_hash = {}, is your logic ok with this?
     if(query_hash.nil? and project_id.nil? and !log_date.nil? )
       daily_logs= DailyLog.where(log_date: log_date).order("log_date DESC")
     end
@@ -19,6 +23,7 @@ class DailyLog < ApplicationRecord
     daily_log_summary
   end
 
+  #TODO create_summary_record would be better name
   def create_record(project_id)
     summary_record=Hash.new
     summary_record[:name] = self.user.name
@@ -26,6 +31,7 @@ class DailyLog < ApplicationRecord
     summary_record[:picture] = self.user.picture
     summary_record[:log_id] = self.id
     summary_record[:takeaway] = self.takeaway.to_s
+    #TODO dont hard code client name values
     summary_record[:learning] = self.log_entries.where(project_id: Project.where(client_name: 'Learning').first.id).first.log_text
     summary_record[:beautifulcode] = beautifulcode_log_record(project_id)
     summary_record[:clients] = self.clients_project_log_record(project_id)
@@ -34,7 +40,9 @@ class DailyLog < ApplicationRecord
   end
 
   def beautifulcode_log_record(project_id)
+    #TODO change company_record name
     company_record={}
+    #TODO why two conditions, project_id.present? should be fine right?
     if(!project_id.nil? and !project_id.blank?)
       project_log_ids = self.log_entries.joins(:project).where(projects:{id: project_id}).pluck(:id,:project_id)
     else
@@ -42,15 +50,22 @@ class DailyLog < ApplicationRecord
     end
     project_log_logtext = Hash.new{ |h,k| h[k] = [] }
     project_log_ids.each do |project_log_ids|
+      #TODO why are you using find_by_id, when you have find()
       project_log_logtext[Project.find(project_log_ids[1]).name] <<[LogEntry.find_by_id(project_log_ids[0]).log_text,project_log_ids[0]]
     end
     project_log_logtext
   end
 
   def clients_project_log_record(project_id)
-    clients=Project.where.not(client_name: ['BeautifulCode', 'Learning']).pluck('distinct client_name')
+    #TODO dont hard code client names
+    clients = Project.where.not(client_name: ['BeautifulCode', 'Learning']).distinct.pluck('client_name')
+    #clients=Project.where.not(client_name: ['BeautifulCode', 'Learning']).pluck('distinct client_name')
+    #TODO no semi colons please
     project_logs=Hash.new();
     clients.each do |client|
+    #TODO why two conditions, project_id.present? should be fine right?
+      ##TODO DONT repeat your code, DRY principle
+      ##TODO combine beautifulcode_log_record and clients_project_log_record, we dont have to show any difference for them
       if(!project_id.nil? and !project_id.blank?)
         project_log_ids= self.log_entries.joins(:project).where(projects:{id: project_id, client_name:'#{client}' }).pluck(:id,:project_id)
       else
@@ -72,8 +87,11 @@ class DailyLog < ApplicationRecord
       end
     end
     self.update_attributes(:takeaway => takeaway)
+    #TODO why are you hard coding project_id value 4
+    ##TODO write a method in project model which return learning projects id
     self.log_entries.find_by_project_id(4).update_attributes(log_text: learning)
   end
+  #TODO add empty line after each method
   def self.build_query_hash(params,current_user_id)
     query_hash ={}
     query_hash[:user_id] = params[:user_id] || current_user_id
